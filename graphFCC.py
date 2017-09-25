f76b595e24f1a6be2ffc1e07672d0e01e3ff2c9f
Zakharov Nikolai <1556712541@qq.com>
2017-09-25 21:43:57 +0800

    optimization for a big graph

--- graphFCC.py
+++ graphFCC.py
@@ -7,15 +7,16 @@
 import collections
 import time
 import sys
+import threading
 import resource
 import os
 
 from kargerMinCut import Graph
 # setting some finite recursion limit size
 # resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
-# sys.setrecursionlimit(8192)
-resource.setrlimit(resource.RLIMIT_STACK, [0x10000000, resource.RLIM_INFINITY])
-sys.setrecursionlimit(0x100000)
+
+# resource.setrlimit(resource.RLIMIT_STACK, [0x10000000, resource.RLIM_INFINITY])
+# sys.setrecursionlimit(0x100000)
 # setting recursion limit to infinity, requires root! can cause stack overflow!
 # resource.setrlimit(resource.RLIMIT_STACK, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
 
@@ -38,33 +39,44 @@ class DirectedGraph(Graph):
     def kosaraju(self):
         self._kosaraju_used = True
         rev_g = self.reversed_graph
+        self.explored = [0] * (len(rev_g) + 1)
         print("Reversed")
         # 1-st pass
         for i in reversed(list(rev_g.keys())):
-            if i not in self.explored:
+            if self.explored[i] != 1:
                 self.s = i
                 self.dfs_loop(i, rev_g)
         del self._reversed_graph
         del rev_g
         print("Second Pass")
         # 2-nd pass
-        self.explored = []
+        self.explored = [0] * (len(self.vertex) + 1)
         self.ld = {}
         for i in reversed(list(self.f_t.keys())):
             cur_vertex = self.f_t[i]
-            if cur_vertex not in self.explored:
+            if self.explored[cur_vertex] != 1:
                 self.s = cur_vertex
                 self.dfs_loop(cur_vertex, self.vertex, True)
 
     def dfs_loop(self, start_v, graph, second_pass=False):
-        self.explored.append(start_v)
+        self.explored[start_v] = 1
         self.ld[start_v] = self.s
         for vtx in graph[start_v]:
-            if vtx not in self.explored:
+            if self.explored[vtx] != 1:
                 self.dfs_loop(vtx, graph, second_pass)
         if not second_pass:
             self.t += 1
             self.f_t[self.t] = start_v
+
+    def dfs(self, start_v, graph):
+        s = collections.deque()
+        s.append(start_v)
+        while s.count >= 1:
+            v = s.pop()
+            if v not in self.explored:
+                self.explored.append(v)
+                for vtx in graph[v]:
+                    s.append(vtx)
 
     @property
     def reversed_graph(self):
@@ -92,10 +104,11 @@ class DirectedGraph(Graph):
         # TODO: compute 5 SCC
         # you ned to run kosaraju algorithm first
         if not self._kosaraju_used:
+            print("use kosaraju")
             self.kosaraju()
         ffc = []
         ffc_num = [0] * (len(self.vertex)+1)
-        for i in list(self.vertex.keys()):
+        for i in list(self.ld.keys()):
             ffc_num[self.ld[i]] += 1
             if self.ld[i] not in ffc:
                 ffc.append(self.ld[i])
@@ -135,13 +148,16 @@ if __name__ == "__main__":
 
     test_cases_path  = "./data/testCases/course2/assignment1SCC"
     # graph3 = DirectedGraph()
-    # graph3.readGraphFromFile(os.path.join(test_cases_path, 'input_mostlyCycles_56_40000.txt'))
+    # graph3.readGraphFromFile(os.path.join(test_cases_path, 'input_mostlyCycles_61_160000.txt'))
     # graph3.kosaraju()
     # print("Number of fully connected componets is: ", graph3.scc())
     graph2 = DirectedGraph()
     graph2.readGraphFromFile('./data/SCC.txt')
     s_t = time.time()
-    graph2.kosaraju()
-    print("Number of fully connected componets is: ", graph2.ffc())
+    sys.setrecursionlimit(2 ** 15)
+    threading.stack_size(67108864)
+    thread = threading.Thread(target=graph2.kosaraju())
+    thread.start()
+    print("Number of fully connected componets is: ", graph2.scc())
     print("Execution time: %.3f seconds" % (time.time() - s_t))
 
